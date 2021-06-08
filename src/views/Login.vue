4c52dfc911ba4f7f77db024ec4ff471992301a2e
vavellar <victorcandeias@wine.com.br>
2021-06-08 07:59:41 -0300

    initial configurations and create component login

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+      <b-jumbotron bg-variant="info" text-variant="white"/>
+      <b-container>
+        <b-row>
+          <b-col lg="4" md="3"></b-col>
+          <b-col lg="4" md="6">
+            <LoginForm/>
+          </b-col>
+          <b-col lg="4" md="3"></b-col>
+        </b-row>
+      </b-container>
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+
+}
+</script>
+
+<style>
+
+</style>
